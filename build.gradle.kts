dddf0255a0ef39cfd5230048c03db136119d3450
JuanTexeira <juansetex@gmail.com>
2023-10-19 23:31:52 -0300

    Se agregan dependencias a modo de prueba para intentar linkear con frontend (marcadas con comentarios)

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
     implementation("org.uqbar-project:geodds-xtend:1.0.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
 
+    //CODIGO HACIENDO PRUEBAS PARA LINKEAR CON EL FRONT
+    implementation ("org.springframework.boot:spring-boot-starter-security:2.5.4")
+
     // testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
--- src/main/kotlin/ar/edu/unsam/algo3/ProyectoApplication.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/ProyectoApplication.kt
@@ -3,9 +3,29 @@ package ar.edu.unsam.algo3
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
+//CODIGO HACIENDO PRUEBAS PARA LINKEAR CON EL FRONT
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.ComponentScans
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+//CODIGO HACIENDO PRUEBAS PARA LINKEAR CON EL FRONT
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4200") // Cambia esto a la URL de tu aplicación Angular
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+            }
+        }
+    }
+}
 
 
 @SpringBootApplication
