e0a990d6fe80910120161db475240df07c976e2e
JuanTexeira <juansetex@gmail.com>
2023-11-13 11:58:46 -0300

    Se modifica crossOrigin a '*' momentáneamente, y se tipan excepciones como 400 y 404

--- src/main/kotlin/ar/edu/unsam/algo3/controller/UsuarioController.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/controller/UsuarioController.kt
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@CrossOrigin("http://localhost:4200/")
+@CrossOrigin("*")
 class UserController(@Autowired val userService : UsuarioService) {
     @GetMapping("/Usuario/")
     fun getUsuario() = userService.getUser()
--- src/main/kotlin/edu/Exceptions.kt
+++ src/main/kotlin/edu/Exceptions.kt
@@ -1,8 +1,22 @@
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.RuntimeException
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 open class genericException(val msg: String) : RuntimeException(msg)
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+class BusinessException(msg: String) : RuntimeException(msg)
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class NotFoundException(msg: String) : RuntimeException(msg)
 class ExceptionNombre(msg: String="El nombre no es valido") :genericException(msg)
 class ExceptionApellido(msg: String="El apellido  no es valido") :genericException(msg)
 
 class InvalidIdException(mensaje: String) : IllegalArgumentException(mensaje)
 
 class InvalidElementException(mensaje: String) : NullPointerException(mensaje)
+
+
+
 
