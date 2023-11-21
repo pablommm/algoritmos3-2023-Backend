77848205ca49767cc101063e31b6e6fd914a89da
francopolesel <francopolesel99@gmail.com>
2023-11-21 15:22:08 -0300

    Se corrigen nombres de HomeService y HomeController

--- src/main/kotlin/ar/edu/unsam/algo3/service/HomeService.kt
+++ src/main/kotlin/ar/edu/unsam/algo3/service/HomeService.kt
@@ -1,14 +1,11 @@
 package ar.edu.unsam.algo3.service
 
-import Entidad
-import ar.edu.unsam.algo3.dto.RepositorioDTO
-import ar.edu.unsam.algo3.dto.toDTO
 import ar.edu.unsam.algo3.repository.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class RepositorioService {
+class HomeService {
 
     @Autowired
     lateinit var repoPuntoDeVentas: RepoPuntoDeVentas
@@ -16,13 +13,13 @@ class RepositorioService {
     @Autowired
     lateinit var repoUser: RepoUser
 
-    data class RepositorioGeneral(
+    data class Home(
         val cantidadPuntosDeVentas: Int,
         val cantidadUsuarios: Int,
         val cantidadFiguritasRepetidas: Int,
         val cantidadFiguritasFaltantes: Int
     )
-    fun getCantidadesHome() = RepositorioGeneral(cantidadPuntosDeVentas = repoPuntoDeVentas.cantidadElementos(),
+    fun getCantidadesHome() = Home(cantidadPuntosDeVentas = repoPuntoDeVentas.cantidadElementos(),
         cantidadUsuarios = repoUser.cantidadElementos(),
         cantidadFiguritasRepetidas = repoUser.allInstances().map { it.figuritasRepetidas.size }.sum(),
         cantidadFiguritasFaltantes = repoUser.allInstances().map { it.figuritasFaltantes.size }.sum()
